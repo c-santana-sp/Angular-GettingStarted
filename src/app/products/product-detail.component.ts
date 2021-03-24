1e7fda6496d7eeb8a63789c21980a6f2cd8bef83
c-santana-sp <carlos.santana@bandtec.com.br>
2021-03-24 13:15:22 -0300

    Creating the back button and setting up routes

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from "@angular/router";
+import { IProduct } from './product';
 
 @Component({
   // selector: 'pm-product-detail',
@@ -7,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product detail";
+  product: IProduct | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) {}
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle = `Product detail: ${id}`;
 
+    this.product = {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    }
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
